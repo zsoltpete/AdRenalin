321bd688d043fa3f50d338325a150cf2f56771fb
Zsolt Pete <pete.zsolt95@gmail.com>
2018-10-14 11:25:40 +0200

    ✨ Added Plane overlay to project

---
+++ AdRenalin/AdRenalin/Detection/DetectionViewController.swift
@@ -0,0 +1,92 @@
+//
+//  DetectionViewController.swift
+//  AdRenalin
+//
+//  Created by Zsolt Pete on 2018. 10. 14..
+//  Copyright © 2018. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class DetectionViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    var planes = [OverlayPlane]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        registerGestureRecognizers()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    private func registerGestureRecognizers() {
+        
+//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+//        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+}
+
+extension DetectionViewController: ARSCNViewDelegate {
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if !(anchor is ARPlaneAnchor) {
+            return
+        }
+        
+        let plane = OverlayPlane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+            }.first
+        
+        if plane == nil {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
+    }
+}
---
+++ AdRenalin/AdRenalin/Utilities/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  AdRenalin
+//
+//  Created by Zsolt Pete on 2018. 10. 14..
+//  Copyright © 2018. Zsolt Pete. All rights reserved.
+//
+
+import Foundation
+
+enum BodyType : Int {
+    case box = 1
+    case plane = 2
+    case car = 3
+}
